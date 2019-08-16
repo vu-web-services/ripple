3818ffc02e88f5ada484b47e9c9b75b77a46edcb
Alan <alan@salsadigital.com.au>
2019-08-16 13:14:45 +1000

    [SDPA-3074] Move winston-logstash-transport into logger.

--- packages/ripple-nuxt-tide/lib/core/logger.js
+++ packages/ripple-nuxt-tide/lib/core/logger.js
@@ -1,5 +1,46 @@
-const { LogstashTransport } = require('winston-logstash-transport')
-const { createLogger, format, transports, addColors } = require('winston')
+const { createLogger, format, transports, addColors, Transport } = require('winston')
+const dgram = require('dgram')
+const os = require('os')
+
+class LogstashTransport extends Transport {
+  constructor(options) {
+    options = options || {}
+    super(options)
+    this.name = 'LogstashTransport'
+    this.host = options.host
+    this.port = options.port
+    this.trailingLineFeed = options.trailingLineFeed === true
+    this.trailingLineFeedChar = options.trailingLineFeedChar || os.EOL
+    this.silent = options.silent
+    this.client = null
+    this.connect()
+  }
+
+  connect() {
+    this.client = dgram.createSocket('udp4')
+    this.client.unref()
+  }
+
+  log(info, callback) {
+    if (this.silent) {
+      return callback(null, true)
+    }
+
+    this.send(info[Symbol.for('message')], (err) => {
+      this.emit('logged', !err)
+      callback(err, !err)
+    })
+  }
+
+  send(message, callback) {
+    if (this.trailingLineFeed === true) {
+      message = message.replace(/\s+$/, '') + this.trailingLineFeedChar
+    }
+
+    const buf = Buffer.from(message)
+    this.client.send(buf, 0, buf.length, this.port, this.host, callback)
+  }
+}
 
 // The logging levels we are using is winston default.
 // https://github.com/winstonjs/winston#logging-levels
